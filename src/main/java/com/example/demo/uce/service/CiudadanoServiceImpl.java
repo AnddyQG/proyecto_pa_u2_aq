5735a369bd5521ee5fa8c4f7cce1d6ddc7495074
Anddy Quisilema <anddysk819@gmail.com>
2023-01-20 19:45:48 -0500

    taller 20

---
+++ src/main/java/com/example/demo/uce/service/CiudadanoServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.demo.uce.service;
+
+import com.example.demo.uce.modelo.Ciudadano;
+
+public class CiudadanoServiceImpl implements ICiudadanoService{
+
+	@Override
+	public void guardar(Ciudadano ciudadano) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

ee57dabb68552ae113e22c03f34b7ad4724e143b
Anddy Quisilema <anddysk819@gmail.com>
2023-01-23 19:43:04 -0500

    Taller 22 hotel y habitacion

---
+++ src/main/java/com/example/demo/uce/service/IEmpleadoService.java
@@ -0,0 +1,12 @@
+package com.example.demo.uce.service;
+
+import com.example.demo.uce.modelo.Empleado;
+
+public interface IEmpleadoService {
+
+	public void agregar(Empleado empleado);
+	public void modificar(Empleado empleado);
+	public Empleado buscar(Integer id);
+	public void eliminar(Integer id);
+	
+}
